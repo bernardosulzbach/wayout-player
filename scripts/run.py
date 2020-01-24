4552601db7309901cea7814ebdef8843bc7141fb
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2020-01-24 15:09:55 -0300

    Running over inputs as the user, not sudo
    Closed #26

--- scripts/run.py
+++ scripts/run.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     for filename in os.listdir(INPUT_PATH):
         print('Working on {}...'.format(filename))
         with open(os.path.join(OUTPUT_PATH, filename), 'w') as output_file:
-            subprocess.run(['systemd-run', '--scope', '-p', 'MemoryMax=16G', './player', os.path.join(INPUT_PATH, filename)], stdout=output_file)
+            subprocess.run(['systemd-run', '--user', '--scope', '-p', 'MemoryMax=16G', './player', os.path.join(INPUT_PATH, filename)], stdout=output_file)
